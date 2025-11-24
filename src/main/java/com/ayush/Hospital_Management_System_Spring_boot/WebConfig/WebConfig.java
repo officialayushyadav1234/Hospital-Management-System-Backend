18214b1728597713ed577cb35c1f9c46a321d0d2
officialayushyadav1234 <officialayushyadav1234@gmail.com>
2025-11-25 04:02:23 +0530

    WebConfig

--- src/main/java/com/ayush/Hospital_Management_System_Spring_boot/WebConfig/WebConfig.java
+++ src/main/java/com/ayush/Hospital_Management_System_Spring_boot/WebConfig/WebConfig.java
@@ -1,4 +1,4 @@
-package com.ayush.Hospital_Management_System_Spring_boot.config; // Check your package name!
+package com.ayush.Hospital_Management_System_Spring_boot.WebConfig; // Check your package name!
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
